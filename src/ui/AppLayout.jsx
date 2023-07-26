f8be9c1ae6fc7ab801fe78ddf9aa5cc7c19938d4
Radu <radu.avadanii@bludelego.it>
2023-07-26 11:01:06 +0300

    feat: Header component.

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+
+function AppLayout() {
+    return (
+        <div>
+            <Header />
+
+            <main>
+                <h1>text</h1>
+            </main>
+        </div>
+    );
+}
+
+export default AppLayout;
