cb37b5ec5c020a4e9f419d03e614db57aa018785
Radu <radu.avadanii@bludelego.it>
2023-07-26 10:37:48 +0300

    feat: routing system.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/menu",
+        element: <Menu />,
+    },
+]);
+
 function App() {
-    return <h1>Title</h1>;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
