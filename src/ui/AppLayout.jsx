aebf00de1b90a16bab8d0d4e3ee2109d6df99cd7
superwo <avadanii@tiscali.it>
2023-07-31 15:58:17 +0300

    feat: added tailwindcss.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,27 @@
-import Header from "./Header";
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
-    return (
-        <div>
-            <Header />
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
 
-            <main>
-                <h1>text</h1>
-            </main>
-        </div>
-    );
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+
+      <Header />
+
+      <div className="overflow-auto">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
