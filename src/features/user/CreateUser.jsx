92b7c323598231f64c62327ae000373caf747bdc
superwo <avadanii@tiscali.it>
2023-08-01 17:23:22 +0300

    lsdh

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react';
-import Button from '../../ui/Button';
+import { useState } from "react";
+import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
+    const [username, setUsername] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
+    function handleSubmit(e) {
+        e.preventDefault();
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <p className="mb-4 text-sm text-stone-600 md:text-base">
-        👋 Welcome! Please start by telling us your name:
-      </p>
+        if (!username) return;
 
-      <input
-        type="text"
-        placeholder="Your full name"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className="input mb-8 w-72"
-      />
+        dispatch(updateName(username));
+        navigate("/menu");
+    }
 
-      {username !== '' && (
-        <div>
-          <Button type="primary">Start ordering</Button>
-        </div>
-      )}
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <p className="mb-4 text-sm text-stone-600 md:text-base">
+                👋 Welcome! Please start by telling us your name:
+            </p>
+
+            <input
+                type="text"
+                placeholder="Your full name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="input mb-8 w-72"
+            />
+
+            {username !== "" && (
+                <div>
+                    <Button type="primary">Start ordering</Button>
+                </div>
+            )}
+        </form>
+    );
 }
 
 export default CreateUser;
